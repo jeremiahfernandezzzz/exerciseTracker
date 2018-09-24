7b43f687b4384d6558e30571846830f5e69b72a6
Glitch (band-booklet) <none>
2018-09-24 07:15:28 +0000

    📫⭐️ Checkpoint ./server.js:491517/218

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ var bodyParser = require('body-parser');
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(bodyParser.json());
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
@@ -18,11 +21,10 @@ app.get('/', function(request, response) {
 });
 
 app.post('/api/exercise/new-user', function(request, response) {
-  response.send(request.body.username);
+  response.send(request.body);
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
-app.use(express.bodyParser());
