a2f908f38967ac1dd1cf3029f4cb5e4e6abdfb23
Glitch (band-booklet) <none>
2018-09-24 07:50:23 +0000

    🍼🖖 Checkpoint ./server.js:491517/44

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.post('/api/exercise/new-user', function(request, response) {
   // response.send(request.body);
       MongoClient.connect(url, function(err, db){
       if (db){
-        db.collection("exercises").insert({username: request.body.username});
-        response.end("aye");
+        // db.collection("exercises").insert({username: request.body.username});
+        response.end(JSON.stringify(db));
       }
       if (err) {
         response.end("did not connect to " + url)
